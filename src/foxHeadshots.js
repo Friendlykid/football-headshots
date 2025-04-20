2dd29112ae369f769a5b389ff63e984b91430eef
Kouba Jiří <jiri.kouba@generaliceska.cz>
2025-04-20 22:41:10 +0200

    better error handling, retry on fail

--- src/foxHeadshots.js
+++ src/foxHeadshots.js
@@ -1,3 +1,4 @@
+import { existsSync } from "fs";
 import path from "path";
 import { appendFile, readFile } from "fs/promises";
 import { chromium } from "playwright";
@@ -5,50 +6,90 @@ import { chromium } from "playwright";
 const sleep = async (time) =>
 	new Promise((resolve) => setTimeout(resolve, time));
 
-const gotoUrl = async (page, url, errorCount = 0, errorMessage = "") => {
-	if (errorCount > 10) return { isError: true, errorMessage };
+const RETRY_COUNT = Number(process.env.RETRY_COUNT ?? 5);
+
+const gotoUrl = async (page, url, name, errorCount = 0, errorMessage = "") => {
+	if (errorCount > RETRY_COUNT)
+		return { isError: true, errorMessage: "Exceeded number of retries" };
 
 	try {
-		await page.goto(url, { timeout: 60_000, waitUntil: "domcontentloaded" });
+		await page.goto(url, { timeout: 30_000, waitUntil: "domcontentloaded" });
 		await page.setViewportSize({ width: 1500, height: 1000 });
 		await page.waitForTimeout(3000);
 		return { isError: false };
 	} catch (e) {
+		if (page.url() === "https://www.foxsports.com/soccer") {
+			await appendFile(
+				path.join(process.cwd(), "data", "links_not_on_fox.csv"),
+				`${name}\n`,
+			);
+			return { isError: true, errorMessage: "does not exist" };
+		}
 		console.log(`Retrying [${errorCount}] for: ${url}`);
+		console.log(page.url());
 		await sleep(1000);
-		return await gotoUrl(page, url, errorCount + 1, e.message);
+		return await gotoUrl(page, url, name, errorCount + 1, e.message);
 	}
 };
 
 const scrapeHeadshot = async (browser, name) => {
 	const page = await browser.newPage();
 	const baseUrl = `https://www.foxsports.com/soccer/${name}-player`;
-	const { isError, errorMessage } = await gotoUrl(page, baseUrl);
-	if (isError) {
-		console.log("skipping ", baseUrl, errorMessage);
+	const { isError, errorMessage = "" } = await gotoUrl(page, baseUrl, name);
+	if (isError || page.url() !== baseUrl) {
+		errorMessage && console.log(errorMessage);
+		await page.close();
 		return;
 	}
-	if (page.url() !== baseUrl) return;
 	const imageLink = await page.evaluate(() => {
-		return document.querySelector("img.image-headshot").getAttribute("src");
+		return document.querySelector("img.image-headshot")?.getAttribute?.("src");
 	});
-	if (imageLink.split("/").pop().startsWith("default-headshot")) return;
-	await gotoUrl(page, imageLink);
+	if (!imageLink || imageLink.split("/").pop().startsWith("default-headshot")) {
+		await page.close();
+		return;
+	}
+	const result = await gotoUrl(page, imageLink);
+
+	if (result.isError) {
+		await page.close();
+		return;
+	}
 	await page.locator("img").screenshot({
 		path: path.join("data", "mls_players", "headshots", `${name}.png`),
 	});
 	await page.close();
 };
 
-const CONCURRENCY_LIMIT = 10;
+const CONCURRENCY_LIMIT = Number(process.env.CONCURRENCY_LIMIT ?? 10);
 
-const main = async () => {
+const main = async (count = 0) => {
+	if (count > 10) return;
 	const browser = await chromium.launch({ headless: true });
 	const linksFile = await readFile(
 		path.join(process.cwd(), "data", "links_deduped.csv"),
 		{ encoding: "utf8" },
 	);
-	const links = linksFile.split("\n").map((link) => link.trim());
+
+	const missingLinksFile = await readFile(
+		path.join(process.cwd(), "data", "links_not_on_fox.csv"),
+		{ encoding: "utf8" },
+	);
+
+	const missingLinks = missingLinksFile.split("\n").map((link) => link.trim());
+
+	const links = linksFile
+		.split("\n")
+		.map((link) => link.trim())
+		.filter(
+			(link) =>
+				link &&
+				!existsSync(
+					path.join("data", "mls_players", "headshots", `${link}.png`),
+				),
+		)
+		.filter((link) => !missingLinks.includes(link));
+	console.log(links.length);
+	if (links.length === 0) return;
 	const chunks = Array.from(
 		{ length: Math.ceil(links.length / CONCURRENCY_LIMIT) },
 		(_, i) =>
@@ -66,6 +107,9 @@ const main = async () => {
 		);
 	}
 	await browser.close();
+	return await main(count + 1);
 };
 
-main();
+main().catch((e) => {
+	console.error("Unexpected error:", e);
+});
